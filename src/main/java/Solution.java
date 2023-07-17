23ed6231e16ebb9ed2af58173f589acd89766e20
alek <wait82985@gmail.com>
2023-07-17 13:12:14 +0600

    add reverse method

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,21 @@
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        //TODO
+
         return null;
+    }
+
+    public ListNode reverse(ListNode root){
+        ListNode previous = null;
+        ListNode current = root;
+        ListNode next = null;
+
+        while (current != null){
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        return  previous;
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -12,6 +12,19 @@ public class SolutionTest {
     }
 
     @Test
+    public void reverseTest(){
+        ListNode l1 = new ListNode(7);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+        l1.next.next.next = new ListNode(3);
+
+        int expected = 3427;
+        int actual = toInteger(new Solution().reverse(l1));
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
     public void addTwoNumbersTest1() {
         ListNode l1 = new ListNode(7);
         l1.next = new ListNode(2);
